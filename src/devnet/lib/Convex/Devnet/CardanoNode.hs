03803df1455c6e2178ee6885940b981c9bf7c135
Jann Müller <jann.mueller@avatar-engines.com>
2026-01-14 08:38:06 +0100

    Fix devnet tests

--- src/devnet/lib/Convex/Devnet/CardanoNode.hs
+++ src/devnet/lib/Convex/Devnet/CardanoNode.hs
@@ -129,6 +129,7 @@ data CardanoNodeArgs = CardanoNodeArgs
   , nodeShelleyGenesisFile :: FilePath
   , nodeAlonzoGenesisFile :: FilePath
   , nodeConwayGenesisFile :: FilePath
+  , nodeDijkstraGenesisFile :: FilePath
   , nodeTopologyFile :: FilePath
   , nodeDatabaseDir :: FilePath
   , nodeDlgCertFile :: Maybe FilePath
@@ -148,6 +149,7 @@ defaultCardanoNodeArgs =
     , nodeShelleyGenesisFile = "genesis-shelley.json"
     , nodeAlonzoGenesisFile = "genesis-alonzo.json"
     , nodeConwayGenesisFile = "genesis-conway.json"
+    , nodeDijkstraGenesisFile = "genesis-dijkstra.json"
     , nodeTopologyFile = "topology.json"
     , nodeDatabaseDir = "db"
     , nodeDlgCertFile = Nothing
@@ -412,6 +414,10 @@ withCardanoNodeDevnetConfig tracer stateDirectory configChanges PortsConfig{ours
       >>= copyAndChangeJSONFile
         cfConway
         (stateDirectory </> nodeConwayGenesisFile args)
+    readConfigFile ("devnet" </> "genesis-dijkstra.json")
+      >>= copyAndChangeJSONFile
+        cfConway
+        (stateDirectory </> nodeDijkstraGenesisFile args)
 
 writeTopology :: [Port] -> FilePath -> CardanoNodeArgs -> IO ()
 writeTopology peers stateDirectory args =
@@ -476,7 +482,7 @@ refreshSystemStart stateDirectory args = do
 -- | Generate a topology file from a list of peers.
 mkTopology :: [Port] -> Aeson.Value
 mkTopology peers =
-  Aeson.object ["Producers" .= map encodePeer peers]
+  Aeson.object ["Producers" .= map encodePeer peers, "localRoots" .= ([] :: [Int]), "publicRoots" .= ([] :: [Int])]
  where
   encodePeer :: Int -> Aeson.Value
   encodePeer port =
